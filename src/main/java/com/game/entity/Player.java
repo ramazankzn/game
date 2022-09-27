348f45645337790f48557a9dfedc1169076334ab
Ramazan Khakimov <ramazankzn@gmail.com>
2022-09-27 09:47:03 +0300

    Initial commit

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,16 @@
+package com.game.entity;
+
+import java.util.Date;
+
+public class Player {
+    private long id;
+    private String name;
+    private String title;
+    private Race race;
+    private Profession profession;
+    private Integer experience;
+    private int level;
+    private int untilNextLevel;
+    private Date birthday;
+    private boolean banned;
+}
